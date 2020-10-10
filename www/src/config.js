5641c7b5718bca307a90bffceb9e207257874297
Leonardo Piedade <leoabrahao@gmail.com>
2020-10-10 14:33:07 -0300

    Testing...

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "mj9hu7v4nrmtsu12cispl3rt2",     // CognitoClientID
-  "api_base_url": "https://1wopya7y2m.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "api_base_url": "https://1wopya7y2m.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "coginto_hosted_domain": "mytodoappdemo-lambda-refarch-webapp2.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d1c5ljh39h40j4.amplifyapp.com"                                      // AmplifyURL
 };
